f2c23d319138328999412af0d07d20e078f1e5bd
GSelvakumar <gselvaofficial@outlook.com>
2021-05-31 08:57:53 +0530

    hand landmarks added

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
 
-    cv2.imshow() # used to display image in the window
+    if results.multi_hand_landmarks:
+        for handlms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handlms)
+
+    cv2.imshow("Image", img) # used to display image in the window
     cv2.waitKey(1) # wait for a pressed key time(ms) if 0 means wait forever
